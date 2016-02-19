529b65e78e93861636d95423987e548103fdd830
lawrence <lawrence.olaiya@andela.com>
2016-02-19 03:57:15 -0800

    Setup application's configuration
    Refers to [##111515918]
    
    Install necessary node modules
    Install necessary bower components
    Setup travisci
    Setup gulpfile
    Setup houndci
    Setup karma and jasmine
    Setup angularjs unit tests
    Setup protractor
    Include jshintrc
    Setup codeclimate
    Setup coveralls
    Add .gitignore
    Add LISENCE
    Add Procfile

---
+++ gulpfile.js
@@ -0,0 +1,168 @@
+var gulp = require('gulp'),
+  less = require('gulp-less'),
+  jade = require('gulp-jade'),
+  bower = require('gulp-bower'),
+  gutil = require('gulp-util'),
+  // concat = require('gulp-concat'),
+  minify = require('gulp-minify'),
+  babelify = require('babelify'),
+  notify = require('gulp-notify'),
+  livereload = require('gulp-livereload'),
+  jshint = require('jshint'),
+  browserify = require('browserify'),
+  path = require('path'),
+  source = require('vinyl-source-stream'),
+  imagemin = require('gulp-imagemin'),
+  nodemon = require('gulp-nodemon'),
+  karma = require('gulp-karma'),
+  protractor = require('gulp-protractor').protractor,
+  paths = {
+    public: 'public/**',
+    jade: ['!app/shared/**', 'app/**/*.jade'],
+    scripts: 'app/**/*.js',
+    main: './app/scripts/application.js',
+    images: 'app/images/**/*',
+    staticFiles: [
+      '!app/**/*.+(less|css|js|jade)',
+      '!app/images/**/*',
+      'app/**/*.*'
+    ],
+    unitTests: [
+      'public/lib/angular/angular.min.js',
+      'public/lib/angular-mocks/angular-mocks.js',
+      'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+      'public/lib/angular-aria/angular-aria.min.js',
+      'public/lib/angular-animate/angular-animate.min.js',
+      'public/lib/angular-material/angular-material.min.js',
+      'public/lib/angular-resource/angular-resource.min.js',
+      'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
+      'public/lib/sweetalert/dist/sweetalert.min.js',
+      'public/lib/ng-file-upload/ng-file-upload.min.js',
+      'public/lib/ng-file-upload-shim/ng-file-upload-shim.min.js',
+      'public/lib/ngprogress/build/ngprogress.min.js',
+      'public/js/bundle.js',
+      'tests/unit/**/*.spec.js'
+    ],
+    libTests: ['lib/tests/**/*.js'],
+    styles: 'app/styles/*.+(less|css)'
+  };
+
+gulp.task('less', () => {
+  gulp.src(paths.styles)
+    .pipe(less({
+      paths: [path.join(__dirname, './app/styles')]
+    }))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('jade', () => {
+  gulp.src(paths.jade)
+    .pipe(jade())
+    .pipe(gulp.dest('./public/'));
+});
+
+gulp.task('images', () => {
+  gulp.src(paths.images)
+    .pipe(imagemin({
+      optimizationLevel: 3,
+      progressive: true,
+      interlaced: true
+    }))
+    .pipe(gulp.dest('./public/images/'));
+});
+
+gulp.task('bower', () => {
+  return bower()
+    .pipe(gulp.dest('public/lib/'));
+});
+
+gulp.task('minifyJs', ['browserify'], () => {
+  gulp.src('./public/js/bundle.js')
+    .pipe(minify())
+    .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('browserify', () => {
+  return browserify({
+      entries: paths.main,
+      debug: true
+    })
+    .transform(babelify, {
+      presets: ['es2015']
+    })
+    .bundle()
+    // vinyl-source-stream makes the bundle compatible with gulp
+    .on('success', gutil.log.bind(gutil, 'Browserify Rebundled'))
+    .on('error', gutil.log.bind(gutil, 'Browserify ' +
+      'Error: in browserify gulp task'))
+    .pipe(source('bundle.js')) // Desired filename
+    // Output the file
+    .pipe(gulp.dest('./public/js/'));
+});
+
+gulp.task('lint', () => {
+  return gulp.src(['./app/**/*.js', './tests/**/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('static-files', () => {
+  return gulp.src(paths.staticFiles)
+    .pipe(gulp.dest('public/'));
+});
+
+gulp.task('nodemon', () => {
+  nodemon({
+      script: 'server.js',
+      ext: 'js',
+      ignore: ['public/', 'node_modules/']
+    })
+    .on('change', ['lint'])
+    .on('restart', () => {
+      // when the app has restarted, run livereload.
+      gulp.src('server.js')
+        .pipe(livereload())
+        .pipe(notify('Reloading. please wait...'));
+    });
+});
+
+gulp.task('test:e2e', (cb) => {
+  return gulp.src(['./tests/e2e/*.js'])
+    .pipe(protractor({
+      configFile: './protractor.conf.js',
+      args: ['--baseUrl', 'http://127.0.0.1:8000']
+    }))
+    .on('error', (e) => {
+      console.log(e);
+    })
+    .on('end', cb);
+});
+
+gulp.task('test:unit', ['browserify'], () => {
+  // Be sure to return the stream
+  return gulp.src(paths.unitTests)
+    .pipe(karma({
+      configFile: __dirname + '/karma.conf.js',
+      // autoWatch: false,
+      // singleRun: true
+      action: 'run'
+    }))
+    .on('error', () => {
+      // Make sure failed tests cause gulp to exit non-zero
+      process.exit(1);
+    });
+});
+
+gulp.task('watch', () => {
+  gulp.watch(paths.jade, ['jade']);
+  gulp.watch(paths.styles, ['less']);
+  gulp.watch(paths.scripts, ['minifyJs']);
+});
+
+gulp.task('build', ['bower', 'jade', 'less', 'static-files',
+  'images', 'minifyJs'
+]);
+
+gulp.task('heroku', ['build']);
+gulp.task('test', ['test:unit', 'test:e2e', 'coveralls']);
+gulp.task('default', ['nodemon', 'watch', 'build']);
---
+++ karma.conf.js
@@ -0,0 +1,127 @@
+// Karma configuration
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files,
+    // exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine', 'sinon'],
+
+    plugins: [
+      'karma-babel-preprocessor',
+      'karma-coverage',
+      'karma-jasmine',
+      'karma-chrome-launcher',
+      'karma-sinon'
+    ],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'public/lib/angular/angular.min.js',
+      'public/lib/angular-mocks/angular-mocks.js',
+      'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+      'public/lib/angular-aria/angular-aria.min.js',
+      'public/lib/angular-route/angular-route.min.js',
+      'public/lib/angular-animate/angular-animate.min.js',
+      'public/lib/angular-material/angular-material.min.js',
+      'public/lib/angular-resource/angular-resource.min.js',
+      'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
+      'public/lib/sweetalert/dist/sweetalert.min.js',
+      'public/lib/ng-file-upload/ng-file-upload.min.js',
+      'public/lib/ng-file-upload-shim/ng-file-upload-shim.min.js',
+      'public/lib/ngprogress/build/ngprogress.min.js',
+      'public/js/bundle.js',
+      'tests/unit/**/*.spec.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/
+    // karma-preprocessor
+    preprocessors: {
+      'public/js/bundle.js': ['coverage'],
+      'tests/unit/**/*.js': ['babel']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl',
+    // 'coverage', 'spec', 'failed'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['coverage', 'progress'],
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR
+    // || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever
+    // any file changes
+    // on true, on Circle CI will break
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers:
+    // https://npmjs.org/browse/keyword/karma-launcher
+    // Options:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari
+    // - PhantomJS
+    // - IE
+    browsers: ['Chrome'], // 'Firefox', 'Safari'],
+
+    // https://www.youtube.com/watch?v=FQwZrOAmMAc
+    // To turn off chrome's security limitations that do
+    // not allow some basics things to run
+    // That are required while developing
+    // customLauncher: {
+    //   chrome_without_security: {
+    //     base: "Chrome",
+    //     flags: ["--disable-web-security"]
+    //   }
+    // }
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false,
+     coverageReporter: {
+      // specify a common output directory
+      dir: 'coverage',
+      reporters: [
+        // reporters not supporting the `file` property
+        { type: 'html', subdir: 'report-html' },
+        { type: 'lcov', subdir: 'report-lcov' },
+        // reporters supporting the `file` property, use `subdir` to directly
+        // output them in the `dir` directory
+        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
+        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
+        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+        { type: 'text', subdir: '.', file: 'text.txt' },
+        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
+      ]
+    }
+  });
+};
