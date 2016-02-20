49adbd35c37ddc1100a8ae45d55d2128c9e25760
lawrence <lawrence.olaiya@andela.com>
2016-02-20 13:10:25 -0800

    Inifnite scroll
    Refers to [#114124433]
    
    Add scroll pagination to user own and featured documents view.

--- app/scripts/controllers/documents.controllers.js
+++ app/scripts/controllers/documents.controllers.js
@@ -16,14 +16,16 @@ export default angular.module('paperless.controllers')
       featuredDocs) => {
 
       $scope.init = () => {
-        $scope.page = 2;
-        $scope.maxPage = ownDocs.data.docs.pages;
+        $scope.nextPageOwn = 2;
+        $scope.maxPageOwn = ownDocs.data.docs.pages;
         $scope.ownDocs = ownDocs.data.docs.docs;
         $scope.noDocsMsg = 'Looks like there\'s nothing here. ' +
           'Kindly create a document to begin.';
         $scope.noFeaturedDocsMsg = 'You currently have no featured documents.';
 
         if ($rootScope.currentUser.role.length > 0) {
+          $scope.nextPageFeatured = 2;
+          $scope.maxPageFeatured = featuredDocs.data.docs.pages;
           $scope.featuredDocs = featuredDocs.data.docs.docs;
         }
       };
@@ -44,16 +46,41 @@ export default angular.module('paperless.controllers')
               html.offsetHeight
             ],
             docHeight = Math.max(...args);
+
           if (currentScrollHeight + docHeight == fixedScrollheight) {
-            var userId = $rootScope.currentUser._id;
-            if ($scope.page <= $scope.maxPage) {
-              $scope.isLoading = true;
-              Documents.userDocs(userId, $scope.page, null, (err, res) => {
-                $scope.isLoading = false;
-                if (err) return console.log(err);
-                $scope.page++;
-                $scope.ownDocs.push(...res.data.docs.docs);
-              });
+            var userId = $rootScope.currentUser._id,
+              loadfeaturedDocsCondition =
+              $scope.maxPageFeatured &&
+              $scope.nextPageFeatured <=
+              $scope.maxPageFeatured,
+              roleId;
+
+            if ($rootScope.currentUser.role.length > 0) {
+              roleId = $rootScope.currentUser.role[0]._id;
+            }
+
+            if ($scope.nextPageOwn <= $scope.maxPageOwn) {
+              $scope.isLoadingOwn = true;
+              Documents
+                .userDocs(userId, $scope.nextPageOwn, null,
+                  (err, res) => {
+                    $scope.isLoadingOwn = false;
+                    if (err) return console.log(err);
+                    $scope.nextPageOwn++;
+                    $scope.ownDocs.push(...res.data.docs.docs);
+                  });
+            }
+
+            if (loadfeaturedDocsCondition) {
+              $scope.isLoadingFeatured = true;
+              Documents
+                .featuredDocs(roleId, $scope.nextPageFeatured, null,
+                  (err, res) => {
+                    $scope.isLoadingFeatured = false;
+                    if (err) return console.log(err);
+                    $scope.nextPageFeatured++;
+                    $scope.featuredDocs.push(...res.data.docs.docs);
+                  });
             }
           }
         });
